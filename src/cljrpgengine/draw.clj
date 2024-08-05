c9f2726d6ef0fc89e3216e1a3999fe01ce58b947
Dan Munro <dan@danmunro.com>
2024-08-05 07:07:34 -0700

    remove comments

--- src/cljrpgengine/draw.clj
+++ src/cljrpgengine/draw.clj
@@ -38,37 +38,8 @@
                                 :frame 0}}
                        :down))))
 
-;(defn update []
-;  (let [elapsed (- (inst-ms (Instant/now)) (:last-frame-change fireas 0))
-;        animation (get-in fireas [:animations (:current-animation fireas)])]
-;    (if (> elapsed (:delay animation))
-;      (update-in fireas :last-frame-change (inst-ms (Instant/now)))))
-;  (println fireas))
-
 (defn draw []
   (q/background 255)
-  ;(println (q/frame-count))
-  ;(println (mod (q/frame-count) 8))
-  ;(if (= (mod (q/frame-count) (:delay (:current-animation fireas))) 0)
-  ;  (update-in fireas [(:current-animation fireas)] assoc :frame inc 0))
-  ;(println @fireas)
-  ;(let [animation ]
-  ;  (println (get-in [:animations animation] @fireas)))
-  ;(println (:current-animation @fireas))
-
-  ;(update-in @fireas [:animations (:current-animation @fireas)] assoc :frame 1)
-
-  ;(if (= (mod (q/frame-count) (:delay (:current-animation fireas))) 0)
-  ;  )
-  ;(println "animations: " (:animations @fireas))
-  ;
-  ;(println "a " ((:current-animation @fireas) (:animations @fireas)))
-  ;
-  ;(System/exit 0)
-  ;(println (get-in @fireas [:animations (:current-animation @fireas) :delay]))
-  ;(println (mod (q/frame-count) (get-in @fireas [:animations (:current-animation @fireas) :frames])))
-
-  ;(fn [_] (mod (q/frame-count) (get-in @fireas [:animations (:current-animation @fireas) :frames])))
   (if (= 0 (mod (q/frame-count) (get-in @fireas [:animations (:current-animation @fireas) :delay])))
     (dosync
       (alter
@@ -79,9 +50,5 @@
   (if (>= (get-in @fireas [:animations (:current-animation @fireas) :frame]) (get-in @fireas [:animations (:current-animation @fireas) :frames]))
     (dosync
       (alter fireas update-in [:animations (:current-animation @fireas) :frame] (fn [_] 0))))
-
-
-  ;(println "b " ((:current-animation @fireas) (:animations @fireas)))
-  ;(System/exit 1)
 
   (sprite/draw @fireas))
