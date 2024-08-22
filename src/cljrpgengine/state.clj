9d3b4d92e8d9ee5a9f877d236b39c1214d304921
Dan Munro <dan@danmunro.com>
2024-08-21 18:28:19 -0700

    use an initial state

--- src/cljrpgengine/state.clj
+++ src/cljrpgengine/state.clj
@@ -6,6 +6,13 @@
             [clojure.string :as string]
             [java-time.api :as jt]))
 
+(def initial-state {:save-name nil
+                    :keys #{}
+                    :mobs #{}
+                    :scene :tinytown
+                    :player nil
+                    :map nil})
+
 (defn- transform-to-save
   [state]
   (let [party (get-in @state [:player :party])]
@@ -28,25 +35,25 @@
 (defn load-save-file
   [save-file]
   (let [data (read-string (slurp save-file))]
-    (ref {:keys #{}
-          :mobs #{}
-          :scene (keyword (:scene data))
-          :save-name (:save-name data)
-          :player (player/create-new-player
-                   (get-in data [:player :party 0 :x])
-                   (get-in data [:player :party 0 :y]))
-          :map (map/load-map (get-in data [:map :name]) (get-in data [:map :room]))})))
+    (ref
+     (merge
+      initial-state
+      {:scene (keyword (:scene data))
+       :save-name (:save-name data)
+       :player (player/create-new-player
+                (get-in data [:player :party 0 :x])
+                (get-in data [:player :party 0 :y]))
+       :map (map/load-map (get-in data [:map :name]) (get-in data [:map :room]))}))))
 
 (defn create-new-state
   []
   (let [player (player/create-new-player 0 0)
         scene (all-scenes/scenes :tinytown)
-        state (ref {:save-name (random-uuid)
-                    :keys #{}
-                    :mobs #{}
-                    :scene :tinytown
-                    :player player
-                    :map nil})]
+        state (ref
+               (merge
+                initial-state
+                {:player player
+                 :save-name (random-uuid)}))]
     (.initialize-scene scene state)
     state))
 
