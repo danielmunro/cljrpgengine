f58d682a53ce96f2f2cf3904a3da59831308c82d
Dan Munro <dan@danmunro.com>
2024-08-31 09:15:05 -0700

    add some items

--- src/cljrpgengine/item.clj
+++ src/cljrpgengine/item.clj
@@ -1,4 +1,21 @@
 (ns cljrpgengine.item)
 
-(def items {:light-health-potion {:name "a potion of light health"}
-            :practice-sword {:name "a wooden practice sword"}})
+(def items {:light-health-potion {:name "a potion of light health"
+                                  :type :consumable
+                                  :affect :restore-hp
+                                  :amount 20
+                                  :worth 5}
+            :practice-sword {:name "a wooden practice sword"
+                             :type :equipment
+                             :position :weapon
+                             :material :wood
+                             :worth 10
+                             :attributes {:slash 1}}
+            :cotton-tunic {:name "a cotton tunic"
+                           :type :equipment
+                           :position :torso
+                           :material :cotton
+                           :worth 12
+                           :attributes {:pierce 1
+                                        :slash 1
+                                        :pierce 1}}})
