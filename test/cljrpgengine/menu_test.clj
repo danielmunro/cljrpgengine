54f9330471d84b4a792beaa50c434b7126641c02
Dan Munro <dan@danmunro.com>
2024-09-02 22:05:48 -0700

    fix test

--- test/cljrpgengine/menu_test.clj
+++ test/cljrpgengine/menu_test.clj
@@ -1,11 +1,11 @@
 (ns cljrpgengine.menu-test
-  (:require [cljrpgengine.menu :as menu]
+  (:require [cljrpgengine.ui :as ui]
             [clojure.test :refer :all]))
 
 (deftest text-fixed-width
   (testing "truncates lines that are too long"
-    (is (= "the great broads... " (#'menu/text-fixed-width "the great broadsword of devestating cleaving" 20))))
+    (is (= "the great broads... " (ui/text-fixed-width "the great broadsword of devestating cleaving" 20))))
   (testing "adds spaces for lines that are too short"
-    (is (= "a potion  " (#'menu/text-fixed-width "a potion" 10))))
+    (is (= "a potion  " (ui/text-fixed-width "a potion" 10))))
   (testing "does not modify a line of perfect length"
-    (is (= "a potion" (#'menu/text-fixed-width "a potion" 8)))))
+    (is (= "a potion" (ui/text-fixed-width "a potion" 8)))))
