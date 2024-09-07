2cce64d581748c6f53357ceff255dfaee503e5c1
Dan Munro <dan@danmunro.com>
2024-09-07 15:16:38 -0700

    item descriptions

--- src/cljrpgengine/item.clj
+++ src/cljrpgengine/item.clj
@@ -1,24 +1,30 @@
 (ns cljrpgengine.item)
 
-(def items {:light-health-potion {:name "a potion of light health"
-                                  :type :consumable
-                                  :affect :restore-hp
-                                  :amount 20
-                                  :worth 5}
-            :practice-sword {:name "a wooden practice sword"
-                             :type :equipment
-                             :position :weapon
-                             :material :wood
-                             :worth 10
-                             :attributes {:slash 1}}
-            :cotton-tunic {:name "a cotton tunic"
-                           :type :equipment
-                           :position :torso
-                           :material :cotton
-                           :worth 12
-                           :attributes {:pierce 1
-                                        :slash 1
-                                        :bash 1}}})
+(def items {:light-health-potion
+            {:name "a potion of light health"
+             :description "Heals 20 hit points."
+             :type :consumable
+             :affect :restore-hp
+             :amount 20
+             :worth 5}
+            :practice-sword
+            {:name "a wooden practice sword"
+             :description "A worn wooden practice sword.  It looks like it's been used by many cadets."
+             :type :equipment
+             :position :weapon
+             :material :wood
+             :worth 10
+             :attributes {:slash 1}}
+            :cotton-tunic
+            {:name "a cotton tunic"
+             :description "A worn cotton tunic."
+             :type :equipment
+             :position :torso
+             :material :cotton
+             :worth 12
+             :attributes {:pierce 1
+                          :slash 1
+                          :bash 1}}})
 
 (defn item-quantity-map
   [items]
--- src/cljrpgengine/menus/item_menu.clj
+++ src/cljrpgengine/menus/item_menu.clj
@@ -7,24 +7,28 @@
 (deftype ItemsMenu [state]
   menu/Menu
   (draw [menu]
-    (ui/draw-window 0 0 (first constants/window) (second constants/window))
-    (ui/draw-cursor 0 0 (inc (ui/get-menu-cursor state (.menu-type menu))))
-    (ui/draw-line 0 0 0 (str (ui/text-fixed-width "Item" constants/item-name-width) " Quantity"))
-    (let [items (:items @state)
+    (let [y (/ (second constants/window) 10)
+          cursor (ui/get-menu-cursor state (.menu-type menu))
+          items (:items @state)
           item-count (count items)]
+      (ui/draw-window 0 0 (first constants/window) (* 9 y))
+      (ui/draw-cursor 0 0 (inc cursor))
+      (ui/draw-line 0 0 0 (str (ui/text-fixed-width "Item" constants/item-name-width) " Quantity"))
       (loop [i 0]
         (if (< i item-count)
-          (let [item (items i)
+          (let [item (get items i)
                 key (:key item)]
             (ui/draw-line
-             0
-             0
-             (inc i)
-             (str (ui/text-fixed-width (item/item-name key) constants/item-name-width) " " (:quantity item))
-             (if (item/is-consumable? key)
-               :font-default
-               :font-disabled))
-            (recur (inc i)))))))
+              0
+              0
+              (inc i)
+              (str (ui/text-fixed-width (item/item-name key) constants/item-name-width) " " (:quantity item))
+              (if (item/is-consumable? key)
+                :font-default
+                :font-disabled))
+            (recur (inc i)))))
+      (ui/draw-window 0 (* 9 y) (first constants/window) y)
+      (ui/draw-line 0 (* 9 y) 0 (:description (item/items (:key (items cursor)))))))
   (cursor-length [_] (count (:items @state)))
   (menu-type [_] :items)
   (key-pressed [_]
