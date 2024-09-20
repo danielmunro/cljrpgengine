45fab70f6fd87d954e0447f6a2cb7210b4aecdc9
Dan Munro <dan@danmunro.com>
2024-09-20 07:04:53 -0700

    remove hardcoded value

--- test/cljrpgengine/player_test.clj
+++ test/cljrpgengine/player_test.clj
@@ -7,21 +7,23 @@
 (deftest test-start-moving
   (testing "can start moving left"
     (let [state (test-util/create-new-state)
-          {{:keys [x y]} :player} @state]
+          {{:keys [x y]} :player
+           {{:keys [tilewidth]} :tileset} :map} @state]
       (player/start-moving!
        state
        :left
-       (+ x 16)
+       (+ x tilewidth)
        y)
       (is (contains? (:keys @state) :left))
       (is (= (get-in @state [:player :party 0 :sprite :current-animation]) :left))))
   (testing "can reset moving"
     (let [state (test-util/create-new-state)
-          {{:keys [x y]} :player} @state]
+          {{:keys [x y]} :player
+           {{:keys [tilewidth]} :tileset} :map} @state]
       (player/start-moving!
        state
        :right
-       (- x 16)
+       (- x tilewidth)
        y)
       (input/key-released! state {:key :right})
       (is (empty? (:keys @state)))
