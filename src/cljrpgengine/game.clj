33d046d7caabcbe9e483894bcf6a6eb25323bf59
Dan Munro <dan@danmunro.com>
2024-11-20 14:55:21 -0800

    incremental work

--- src/cljrpgengine/game.clj
+++ src/cljrpgengine/game.clj
@@ -1,10 +1,18 @@
 (ns cljrpgengine.game
-  (:require [cljrpgengine.screens.main-menu :as main-menu])
-  (:import [com.badlogic.gdx Game]))
+  (:require [cljrpgengine.deps :as deps]
+            [cljrpgengine.screens.main-menu :as main-menu])
+  (:import [com.badlogic.gdx Game]
+           (com.badlogic.gdx.graphics.g2d BitmapFont SpriteBatch)))
 
 (gen-class
   :name "cljrpgengine.game.Game"
   :extends com.badlogic.gdx.Game)
 
 (defn -create [^Game this]
+  (swap! deps/batch (constantly (SpriteBatch.)))
+  (swap! deps/font (constantly (BitmapFont.)))
   (.setScreen this (main-menu/screen this)))
+
+(defn -dispose [^Game _]
+  (.dispose @deps/batch)
+  (.dispose @deps/font))
--- src/cljrpgengine/screens/main_menu.clj
+++ src/cljrpgengine/screens/main_menu.clj
@@ -4,26 +4,44 @@
            [com.badlogic.gdx.graphics Color GL20]
            (com.badlogic.gdx.graphics.g2d BitmapFont)
            (com.badlogic.gdx.scenes.scene2d Stage)
-           (com.badlogic.gdx.scenes.scene2d.ui Label Label$LabelStyle)))
+           (com.badlogic.gdx.scenes.scene2d.ui Label Label$LabelStyle)
+           (com.badlogic.gdx.utils ScreenUtils)))
 
 (defn screen
   [game]
-  (println game)
-  (let [stage (atom nil)]
+  (let [stage (atom nil)
+        ;menu (cljrpgengine.menus.main.main-menu/create-menu)
+        dispose (fn []
+                  (println "in dispose")
+                  (.dispose @deps/batch)
+                  (.dispose @deps/font))]
     (proxy [Screen] []
       (show []
+        (println "show")
         (reset! stage (Stage.))
         (let [style (Label$LabelStyle. (BitmapFont.) (Color. 1 1 1 1))
               label (Label. "Hello world!" style)]
-          (.addActor @stage label)))
+          (.addActor @stage label))
+        )
       (render [delta]
-        (.glClearColor (Gdx/gl) 0 0 0 0)
-        (.glClear (Gdx/gl) GL20/GL_COLOR_BUFFER_BIT)
+        ;(.glClearColor (Gdx/gl) 0 0 0 0)
+        ;(.glClear (Gdx/gl) GL20/GL_COLOR_BUFFER_BIT)
+        (ScreenUtils/clear Color/BLACK)
         (.apply deps/viewport)
+
+        ;(.setProjectionMatrix @deps/batch (.combined (.getCamera deps/viewport)))
+        ;(.begin @deps/batch)
+        ;(.draw @deps/font @deps/batch "Welcome to my game" (float 1.0) (float 1.5))
+        ;(.draw @deps/font @deps/batch "Tap anywhere yo" (float 1.0) (float 1.0))
+        ;(.end @deps/batch)
+        ;(if (.isTouched Gdx/input)
+        ;  (dispose))
+
         (doto @stage
           (.act delta)
           (.draw)))
-      (dispose [])
+      (dispose []
+        (dispose))
       (hide [])
       (pause [])
       (resize [w h]
