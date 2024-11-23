e2edd6df61a63141a2c782dae4a5cb81419f722b
Dan Munro <dan@danmunro.com>
2024-11-22 18:26:01 -0800

    walking animation

--- src/cljrpgengine/screens/dungeon.clj
+++ src/cljrpgengine/screens/dungeon.clj
@@ -1,23 +1,40 @@
 (ns cljrpgengine.screens.dungeon
-  (:require [cljrpgengine.deps :as deps])
+  (:require [cljrpgengine.constants :as constants]
+            [cljrpgengine.deps :as deps]
+            [clojure.java.io :as io])
   (:import (com.badlogic.gdx Screen)
-           (com.badlogic.gdx.graphics Color )
+           (com.badlogic.gdx.files FileHandle)
+           (com.badlogic.gdx.graphics Color Texture)
+           (com.badlogic.gdx.graphics.g2d Animation Animation$PlayMode TextureRegion)
            (com.badlogic.gdx.scenes.scene2d Stage)
-           (com.badlogic.gdx.utils ScreenUtils)))
+           (com.badlogic.gdx.utils Array ScreenUtils)))
 
 (defn screen
   [game]
   (let [stage (atom nil)
         dispose (fn []
                   (.dispose @deps/batch)
-                  (.dispose @deps/font))]
+                  (.dispose @deps/font))
+        tx (Texture. (FileHandle. (io/file "resources/sprites/edwyn.png")))
+        txr (TextureRegion/split tx 16 24)
+        walk-frames (object-array [(-> txr (get 0) (get 0))
+                                   (-> txr (get 0) (get 1))
+                                   (-> txr (get 0) (get 0))
+                                   (-> txr (get 0) (get 2))])
+        down (Animation. 0.1 (Array/with walk-frames) Animation$PlayMode/LOOP)
+        state-time (atom (float 0))]
     (proxy [Screen] []
       (show []
-        (reset! stage (Stage.)))
+        #_(reset! stage (Stage.)))
       (render [delta]
         (ScreenUtils/clear Color/BLACK)
         (.apply deps/viewport)
-        (doto @stage
+        (swap! state-time (fn [t] (+ t delta)))
+        (let [frame (.getKeyFrame down @state-time true)]
+          (.begin @deps/batch)
+          (.draw @deps/batch frame (float (- (/ constants/screen-width 2) 8)) (float (- (/ constants/screen-height 2) 12)))
+          (.end @deps/batch))
+        #_(doto @stage
           (.act delta)
           (.draw)))
       (dispose []
