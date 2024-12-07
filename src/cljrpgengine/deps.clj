633a8a82f950b9d9dd389a5a399f68bd6ae0e1b0
Dan Munro <dan@danmunro.com>
2024-12-07 09:22:19 -0800

    move dependency initialization

--- src/cljrpgengine/deps.clj
+++ src/cljrpgengine/deps.clj
@@ -1,7 +1,28 @@
-(ns cljrpgengine.deps)
+(ns cljrpgengine.deps
+  (:require [cljrpgengine.constants :as constants])
+  (:import (com.badlogic.gdx.files FileHandle)
+           (com.badlogic.gdx.graphics Color OrthographicCamera)
+           (com.badlogic.gdx.graphics.g2d SpriteBatch)
+           (com.badlogic.gdx.graphics.g2d.freetype FreeTypeFontGenerator FreeTypeFontGenerator$FreeTypeFontParameter)
+           (com.badlogic.gdx.graphics.glutils ShapeRenderer)
+           (com.badlogic.gdx.utils.viewport FitViewport)))
 
 (def batch (atom nil))
 (def font (atom nil))
 (def camera (atom nil))
 (def viewport (atom nil))
 (def shape (atom nil))
+
+(defn init-dependencies
+  []
+  (swap! batch (constantly (SpriteBatch.)))
+  (swap! camera (constantly (OrthographicCamera.)))
+  (swap! viewport (constantly (FitViewport. (/ constants/screen-width constants/tile-size)
+                                            (/ constants/screen-height constants/tile-size)
+                                            camera)))
+  (swap! shape (constantly (ShapeRenderer.)))
+  (let [generator (FreeTypeFontGenerator. (FileHandle. ^String constants/font-file))
+        parameters (FreeTypeFontGenerator$FreeTypeFontParameter.)]
+    (set! (. parameters -size) constants/font-size)
+    (set! (. parameters -color) Color/WHITE)
+    (swap! font (constantly (.generateFont generator parameters))))
--- src/cljrpgengine/game.clj
+++ src/cljrpgengine/game.clj
@@ -1,32 +1,15 @@
 (ns cljrpgengine.game
-  (:require [cljrpgengine.constants :as constants]
-            [cljrpgengine.deps :as deps]
+  (:require [cljrpgengine.deps :as deps]
             [cljrpgengine.item :as item]
             [cljrpgengine.screens.main-menu :as main-menu])
-  (:import [com.badlogic.gdx Game]
-           (com.badlogic.gdx.files FileHandle)
-           (com.badlogic.gdx.graphics Color OrthographicCamera)
-           (com.badlogic.gdx.graphics.g2d SpriteBatch)
-           (com.badlogic.gdx.graphics.g2d.freetype FreeTypeFontGenerator FreeTypeFontGenerator$FreeTypeFontParameter)
-           (com.badlogic.gdx.graphics.glutils ShapeRenderer)
-           (com.badlogic.gdx.utils.viewport FitViewport)))
+  (:import [com.badlogic.gdx Game]))
 
 (gen-class
  :name "cljrpgengine.game.Game"
  :extends com.badlogic.gdx.Game)
 
 (defn -create [^Game this]
-  (swap! deps/batch (constantly (SpriteBatch.)))
-  (swap! deps/camera (constantly (OrthographicCamera.)))
-  (swap! deps/viewport (constantly (FitViewport. (/ constants/screen-width constants/tile-size)
-                                                 (/ constants/screen-height constants/tile-size)
-                                                 @deps/camera)))
-  (swap! deps/shape (constantly (ShapeRenderer.)))
-  (let [generator (FreeTypeFontGenerator. (FileHandle. constants/font-file))
-        parameters (FreeTypeFontGenerator$FreeTypeFontParameter.)]
-    (set! (. parameters -size) constants/font-size)
-    (set! (. parameters -color) Color/WHITE)
-    (swap! deps/font (constantly (.generateFont generator parameters))))
+  (deps/init-dependencies)
 
   (item/load-items!)
 
